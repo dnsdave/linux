15f828608bc90e0394df81905691f382a2d0a218
dnsdave <30662565+dnsdave@users.noreply.github.com>
2019-11-01 10:17:04 -0400

    moved to snippets

--- snippets/installwebmin.sh
+++ snippets/installwebmin.sh
@@ -1,21 +1,8 @@
 !#/bin/bash
-
-# Install Webmin to a Local Machine
+#Install Webmin to a Local Machine
 
 add-apt-repository 'deb http://download.webmin.com/download/repository sarge contrib'
-
-sleep .5
-
 wget http://www.webmin.com/jcameron-key.asc
-
-sleep .5
-
 apt-key add jcameron-key.asc
-
-sleep .5
-
 apt-get update
-
-sleep .5
-
 apt-get install -y webmin
